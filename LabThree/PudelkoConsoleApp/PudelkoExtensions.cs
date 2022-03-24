f06302fd70263e3ece5fe793bed65e2c0b8b6caf
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-03-24 16:23:27 +0100

    end

---
+++ LabThree/PudelkoConsoleApp/PudelkoExtensions.cs
@@ -0,0 +1,11 @@
+﻿using LabThree;
+
+namespace PudelkoConsoleApp; 
+
+public static class PudelkoExtensions {
+    public static Pudelko Compress(this Pudelko box) {
+        var a = Math.Pow((double) box.Objetosc, (1 / 3));
+        var param = Convert.ToDecimal(a);
+        return new Pudelko(param, param, param);
+    }
+}
