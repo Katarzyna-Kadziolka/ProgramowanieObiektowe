355664bdf8a19b55115b37e4a2ae6b61b4e3a68f
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-04-07 18:30:19 +0200

    bit array

---
+++ LabFour/BitArray/BitArray/BitMatrix.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+
+namespace BitArray; 
+
+public class BitMatrix: IEnumerable<bool> {
+    private System.Collections.BitArray[] data;
+    public int Dimension => data.Length; 
+    public BitMatrix(int n) {
+        data = new System.Collections.BitArray [n];
+        for (int i = 0; i < n; i++) {
+            data[i] = new System.Collections.BitArray(n);
+        }
+    }
+
+    public bool this[int i, int j] {
+        get => data[i][j];
+        set => data[i][j] = value;
+    }
+
+    public override string ToString() {
+        string result = "";
+        foreach (var row in data) {
+            foreach (bool cell in row) {
+                result += cell + " ";
+            }
+
+            result += "\n";
+        }
+
+        return result;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+        return GetEnumerator();
+    }
+    
+    public IEnumerator<bool> GetEnumerator() {
+        foreach (var row in data) {
+            foreach (bool cell in row) {
+                yield return cell;
+            }
+        }
+    }
+
+    public BitMatrix And(BitMatrix other) {
+        foreach (var row in other) {
+            foreach (bool cell in row) {
+                throw new NotImplementedException();
+            }
+        }
+    }
+    
+    public BitMatrix Or(BitMatrix other) {
+        throw new NotImplementedException();
+    }
+    
+    public BitMatrix Not(BitMatrix other) {
+        throw new NotImplementedException();
+    }
+
+    public static BitMatrix operator &(in BitMatrix a, in BitMatrix b) {
+        if (a.Dimension != b.Dimension) {
+            throw new ArgumentException();
+        }
+
+        return new BitMatrix(a.Dimension);
+    }
+}
---
+++ LabFour/BitArray/BitArray/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using BitArray;
+
+BitMatrix matrix = new BitMatrix(5);
+Console.WriteLine(matrix);
+Console.WriteLine(matrix[2, 1]);
+matrix[2, 1] = true;
+Console.WriteLine(matrix);
+foreach (var bit in matrix) {
+    Console.WriteLine(bit);
+}
